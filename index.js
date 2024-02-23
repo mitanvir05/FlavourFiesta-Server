c4578e9d968326f2ab2b9311ea81159a8bf7021f
mitanvir05 <tanvir.cse05@gmail.com>
2024-02-24 03:23:53 +0600

    review data added

--- index.js
+++ index.js
@@ -27,9 +27,16 @@ async function run() {
     await client.connect();
 
     const menuCollection = client.db("flavourFiestaDb").collection("menu");
+    const reviewsCollection = client.db("flavourFiestaDb").collection("reviews");
 
     app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray();
+            res.send(result);
+    
+    });
+
+    app.get('/reviews', async (req, res) => {
+            const result = await reviewsCollection.find().toArray();
             res.send(result);
     
     });
